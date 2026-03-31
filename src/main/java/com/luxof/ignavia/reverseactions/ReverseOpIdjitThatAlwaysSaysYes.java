f88a5f869fee87d75544292726340d79c933b62f
Real-Luxof <luxtheluxof@gmail.com>
2026-03-31 13:41:17 +0500

    pre-lock in intermission

--- src/main/java/com/luxof/ignavia/reverseactions/ReverseOpIdjitThatAlwaysSaysYes.java
+++ src/main/java/com/luxof/ignavia/reverseactions/ReverseOpIdjitThatAlwaysSaysYes.java
@@ -1,15 +1,16 @@
 package com.luxof.ignavia.reverseactions;
 
-import java.util.List;
-
-import com.luxof.ignavia.nocarpaltunnel.ConstMediaActionNCT;
-import com.luxof.ignavia.nocarpaltunnel.HexIotaStack;
-
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
 import at.petrak.hexcasting.api.casting.iota.BooleanIota;
 import at.petrak.hexcasting.api.casting.iota.Iota;
 
+import com.luxof.ignavia.nocarpaltunnel.ConstMediaActionNCT;
+import com.luxof.ignavia.nocarpaltunnel.HexIotaStack;
+
+import java.util.List;
+
 // he's an idjit, but he's MY idjit.
+// no homo
 public class ReverseOpIdjitThatAlwaysSaysYes extends ConstMediaActionNCT {
     public int argc = 0;
     public long mediaCost = 0L;
